82b3a9e4bb444b41094ec489f21c89d74cc1dba9
makarman <makarkarm@gmail.com>
2018-08-21 13:35:10 +0300

    fixed

--- Makar/demon.py
+++ Makar/demon.py
@@ -1,34 +1,45 @@
-from threading import Thread
-from random import randint
-import time
+# Деякі потоки виконують фонові завдання, 
+# наприклад, відправляючи 
+# виконуючи періодичну збирання сміття, 
+# або що завгодно. Вони корисні лише тоді, коли працює основна програма, 
+# і їх можна вбити, коли інші, не демон, потоки вийшли.
+
+# Без потоків daemon, вам доведеться стежити за ними і 
+# повідомити їм, щоб вони вийшли, перш ніж ваша програма може повністю вийти. 
+# Встановивши їх як потоки daemon, ви можете дозволити їм запустити і забути про них, 
+# і коли ваша програма закриється, всі потоки daemon будуть автоматично вбиті.
+
+
+
 import sys
+import threading
+import time
+import logging
 
+logging.basicConfig(level=logging.DEBUG,
+                    format='(%(threadName)-10s) %(message)s',
+                    )
 
-class MyThread(Thread):
-    a = 123
-    def __init__(self, val):
-        ''' Constructor. '''
-        Thread.__init__(self)
-        self.val = val
-    def run(self):
-        for i in range(1, self.val):
-            with open("demofile.txt", "a") as f:
-                f.write("Now the file has one more line! \n") 
-            secondsToSleep = randint(1, 5)
-            print('%s sleeping fo %d seconds...' % (self.getName(), secondsToSleep))
-            time.sleep(secondsToSleep)
-# Run following code when the program starts
-   # Declare objects of MyThread class
-# my_thread_obj_one = MyThread(4)
-# my_thread_obj_one.setName('Thread 1')
-my_thread_obj_two = MyThread(1)
-my_thread_obj_two.setName('Thread 2')
-my_thread_obj_two.setDaemon(True)
-my_thread_obj_two.start()
-my_thread_obj_two.join()
+def daemon():
+    logging.debug('Starting')
+    # time.sleep(1)
+    f = open("abc.txt", "a")
+    f.write("!!!!!!") 
+    logging.debug('Exiting')
 
-# Start running the threads!
-sys.exit()
-# Wait for the threads to finish...
-# my_thread_obj_one.join()# як даний (гобальний потік) блокується і чекає завершення поточного потоку
-# my_thread_obj_two.join()
+d = threading.Thread(name='daemon', target=daemon)
+d.setDaemon(True)
+
+def non_daemon():
+    logging.debug('Starting')
+    f = open("abc.txt", "a")
+    f.write("abc!") 
+    logging.debug('Exiting')
+
+t = threading.Thread(name='non-daemon', target=non_daemon)
+
+d.start()
+t.start()
+# t.join()
+# d.join()
+
